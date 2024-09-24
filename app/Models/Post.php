d85e1c74310868721d5b58d837672b33697d073e
maansi <maansi.bmninfotech13@gmail.com>
2024-09-24 22:51:58 +0530

    Sorted issue of Post Trait

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -10,13 +10,24 @@ class Post extends Model
     use
         SoftDeletes,
         \App\Traits\PostHaveRelations,
-        \App\Traits\PostHaveCustomAttrbutes,
+        \App\Traits\PostHaveCustomAttributes,
         \App\Traits\PostHaveCustomMutators;
 
     protected $fillable = [
-        'title', 'description','address','country_id', 'user_phone_id', 'created_by_user_id', 'purpose_id',
-        'level_id', 'gender_preference', 'status', 'min_budget',
-        'max_budget', 'budget_currency_code',
+        'title',
+        'description',
+        'address',
+        'country_id',
+        'user_phone_id',
+        'created_by_user_id',
+        'purpose_id',
+        'level_id',
+        'gender_preference',
+        'status',
+        'min_budget',
+        'max_budget',
+        'budget_currency_code',
     ];
 
+    protected $appends = ['budget', 'posted_since', 'impressions_count'];
 }
--- app/Traits/PostHaveCustomAttributes.php
+++ app/Traits/PostHaveCustomAttributes.php
@@ -5,18 +5,21 @@ namespace App\Traits;
 use Carbon\CarbonInterface;
 use Illuminate\Support\Carbon;
 
-trait PostHaveCustomAttrbutes{
-    protected $appends = ['budget', 'posted_since', 'impressions_count'];
+trait PostHaveCustomAttributes
+{
 
-    public function getBudgetAttribute(){
+    public function getBudgetAttribute()
+    {
         return intval($this->min_budget) . " - " . intval($this->max_budget);
     }
 
-    public function getPostedSinceAttribute(){
+    public function getPostedSinceAttribute()
+    {
         return Carbon::parse($this->created_at)->diffForHumans(now(), CarbonInterface::DIFF_RELATIVE_TO_NOW);
     }
 
-    public function getImpressionsCountAttribute(){
+    public function getImpressionsCountAttribute()
+    {
         return $this->impressions()->count();
     }
 }
